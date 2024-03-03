63fcb6b5cda49e5b920b21c0bb9db4df679fc92c
Ravi Mahajan <ravi.d.mahajan@gmail.com>
2024-03-03 13:34:13 +0530

    non-primitive types

--- src/index.js
+++ src/index.js
@@ -23,6 +23,7 @@ var collection4 = new Array();
 collection4[0] = 5;
 collection4[1] = "B";
 console.log(collection4);
+console.log("------------------------------------");
 // destructuring before
 var collection5 = [10, "C", true];
 var num = collection5[0];
@@ -32,6 +33,7 @@ var isAvailable = collection5[2];
 var collection5 = [10, "C", true];
 var n = collection5[0], Pname = collection5[1], Flag = collection5[2];
 console.log("Number= ".concat(num, " \nName= ").concat(Pname, " \nIs Available = ").concat(Flag));
+console.log("------------------------------------");
 // Function in an array
 var arr = [10, "C", true, ["Mumbai", "Chennai"], function () { console.log("Function in an array"); }];
 console.log(arr[3][0]);
@@ -52,9 +54,74 @@ var categories = ["Electronics", "Footwear", "Jewellery"];
 for (var property in categories) {
     console.log("".concat(property, " - ").concat(categories[property], " "));
 }
+console.log("------------------------------------");
 //union of types
 // var collections: string[] | number[] = [10, "A", 20];
 var collections = [];
 collections[0] = 10;
 collections[1] = "A";
 collections[2] = 20;
+// Object in TS
+var person = {
+    name: "John Doe",
+    age: 35
+    // gender:  'Male' // Not allowed as it is not part of the interface
+};
+console.log("Name is ".concat(person.name));
+console.log("Age is ".concat(person.age));
+// optional parameter
+var person1 = {
+    name: "John Doe",
+    age: 35
+    // gender:  'Male' // Not allowed as it is not part of the interface
+};
+console.log("Name is ".concat(person1.name));
+console.log("Age is ".concat(person1.age));
+console.log("------------------------------------");
+// function in object , optional function
+var car = {
+    Name: "Tesla",
+    Price: 89764,
+    Qty: 2,
+    Total: function () { return this.Price * this.Qty; },
+    Print: function () { console.log("\nName:".concat(this.Name, "\nPrice:").concat(this.Price, "\nQuantity:").concat(this.Qty, "\nTotal = ").concat(this.Total())); }
+};
+car.Print();
+console.log("------------------------------------");
+var car1 = {
+    Name: "Tesla",
+    Price: 89764,
+    Cities: ["Mumbai", "Delhi"],
+    Rating: { Rate: 5.0, Count: 1000 },
+    Qty: 2,
+    Total: function () { return this.Price * this.Qty; },
+    Print: function () {
+        console.log("\n    Name =".concat(this.Name, "\n\n    Price=").concat(this.Price, "\n\n    Quantity=").concat(this.Qty, "\n\n    Total= ").concat(this.Total(), "\n\n    Cities: ").concat(this.Cities.toString(), "\n\n    Rating= ").concat(this.Rating.Rate, " [").concat(this.Rating.Count, "]"));
+    }
+};
+car1.Print();
+console.log("------------------------------------");
+// Array of Objects
+var students = [{ Name: "John", Age: 23 }, { Name: "Jane", Age: 21 }];
+for (var _a = 0, students_1 = students; _a < students_1.length; _a++) {
+    var student = students_1[_a];
+    console.log("".concat(student.Name, " - ").concat(student.Age));
+}
+console.log("------------------------------------");
+//Map Type
+var data = new Map();
+data.set('India', 12);
+data.set("USA", 3);
+console.log(data);
+console.log("------------------------------------");
+var newData = new Map();
+newData.set('India', 12);
+newData.set(3, "USA");
+console.log(newData);
+console.log("------------------------------------");
+// date type
+var Mfg = new Date();
+console.log("Mfg = " + Mfg.toLocaleDateString());
+console.log("------------------------------------");
+var Mfg1 = new Date("2024-05-06 10:20:32.99");
+console.log("Mfg = " + Mfg1.toLocaleDateString());
--- src/index.ts
+++ src/index.ts
@@ -33,7 +33,7 @@ collection4[0] = 5;
 collection4[1] = "B";
 console.log(collection4);
 
-
+console.log("------------------------------------");
 
 // destructuring before
 var collection5: any[] = [10, "C", true];
@@ -46,6 +46,8 @@ var isAvailable = collection5[2];
 var collection5: any[] = [10, "C", true];
 var [n, Pname, Flag] = collection5;
 console.log(`Number= ${num} \nName= ${Pname} \nIs Available = ${Flag}`);
+
+console.log("------------------------------------");
 
 // Function in an array
 var arr: any[] = [10, "C", true, ["Mumbai", "Chennai"], function(){console.log("Function in an array")}];
@@ -71,7 +73,7 @@ var categories:string[] = ["Electronics", "Footwear", "Jewellery"];
 for(var property in categories){
     console.log(`${property} - ${categories[property]} `);
 }
-
+console.log("------------------------------------");
 
 
 //union of types
@@ -81,3 +83,93 @@ var collections: string[] | number[] = [];
 collections[0] = 10;
 collections[1] = "A";
 collections[2] = 20;
+
+// Object in TS
+
+let  person : {name: string , age:number}  ={
+    name:"John Doe" ,
+    age:35
+    // gender:  'Male' // Not allowed as it is not part of the interface
+    };
+
+console.log(`Name is ${person.name}`);
+console.log(`Age is ${person.age}`);
+
+
+// optional parameter
+let  person1 : {name: string , age:number, gender?:string}  ={
+    name:"John Doe" ,
+    age:35
+    // gender:  'Male' // Not allowed as it is not part of the interface
+    };
+
+console.log(`Name is ${person1.name}`);
+console.log(`Age is ${person1.age}`);
+console.log("------------------------------------");
+
+// function in object , optional function
+let car: {Name: string, Price: number, Qty: number,  Total: any, Print?: any} = {
+    Name: "Tesla",
+    Price:  89764,
+    Qty:    2,
+    Total: function() { return this.Price * this.Qty },
+    Print: function () { console.log (`\nName:${this.Name}\nPrice:${this.Price}\nQuantity:${this.Qty}\nTotal = ${this.Total()}`) }
+}
+
+car.Print();
+console.log("------------------------------------");
+
+let car1: {Name: string, Price: number, Qty: number, Cities: string[], Rating: {Rate: number, Count: number},  Total: any, Print: any} = {
+    Name: "Tesla",
+    Price:  89764,
+    Cities: ["Mumbai", "Delhi"],
+    Rating: { Rate: 5.0, Count: 1000},
+    Qty:    2,
+    Total: function() { return this.Price * this.Qty },
+    Print: function () { 
+    console.log (`
+    Name =${this.Name}\n
+    Price=${this.Price}\n
+    Quantity=${this.Qty}\n
+    Total= ${this.Total()}\n
+    Cities: ${this.Cities.toString()}\n
+    Rating= ${this.Rating.Rate} [${this.Rating.Count}]`) }
+}
+
+car1.Print();
+console.log("------------------------------------");
+
+// Array of Objects
+
+let students: {Name: string, Age: number}[] = [{Name: "John", Age: 23}, { Name: "Jane", Age: 21}];
+
+for(var student of students){
+    console.log(`${student.Name} - ${student.Age}`);
+}
+
+
+console.log("------------------------------------");
+
+//Map Type
+// to use run -> npm i @types/node
+let data: Map<string,number> = new Map();
+data.set('India', 12)
+data.set("USA", 3);
+console.log(data);
+
+console.log("------------------------------------");
+
+let newData: Map<any,any> = new Map();
+newData.set('India', 12);
+newData.set(3, "USA");
+console.log(newData);
+console.log("------------------------------------");
+
+
+// date type
+
+let Mfg = new Date();
+console.log("Mfg = " + Mfg.toLocaleDateString());
+console.log("------------------------------------");
+let Mfg1 = new Date("2024-05-06 10:20:32.99");
+console.log("Mfg = " + Mfg1.toLocaleDateString());
